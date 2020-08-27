36b6a721d2663b3deffe3f44358a6f7cd81d189d
Vlad Fishbain <vlad_fb@mail.ru>
2020-08-27 10:24:47 +0300

    Initial commit

---
+++ FileReader.xaml.cs
@@ -0,0 +1,167 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace MyApp
+{
+    /// <summary>
+    /// Interaction logic for FileReader.xaml
+    /// </summary>
+    public partial class FileReader : Window
+    {
+        String filepath;
+        string[] help;
+        int firstIndex = 0;
+        int secondIndex = 0;
+        public FileReader(String filepath)
+        {
+            if (System.IO.File.Exists(filepath) && filepath != null)
+            {
+                this.filepath = filepath;
+                InitializeComponent();
+                Upload();
+            }
+        }
+        public void Upload()
+        {
+            lbox1.ItemsSource = System.IO.File.ReadAllLines("min_help.txt");
+            string s = System.IO.File.ReadAllText(filepath);
+            List<string> result = new List<string>();
+            Regex regular = new Regex(".{0,50}(?=\\s|$)", RegexOptions.Singleline);
+            MatchCollection matches = regular.Matches(s);
+            foreach (Match match in matches)
+            {
+                string a = match.Value.Trim();
+                a = a.Replace("\n"," ");
+                a = a.Replace("\r", " ");
+                result.Add(a);
+            }
+            foreach (string i in result)
+            {
+                lbox2.Items.Add(i);
+            }
+            help = System.IO.File.ReadAllLines("min_help.txt");
+            for (int i = 0; i < help.Length; i++)
+            {
+                help[i] = help[i].Split('\t')[0];
+            }
+            lbox3.Visibility = Visibility.Hidden;
+            lbox4.Visibility = Visibility.Hidden;
+            tb1.Visibility = Visibility.Hidden;
+            bt1.Visibility = Visibility.Hidden;
+        }
+
+        private void Lbox2_MouseDoubleClick(object sender, MouseButtonEventArgs e)
+        {
+            lbox3.Items.Clear();
+            string str = ((sender as ListBox).SelectedItem as string);
+            firstIndex = (sender as ListBox).SelectedIndex;
+            foreach (string i in str.Split(' '))
+            {
+                if(i.Trim().Length>2)
+                lbox3.Items.Add(i.Trim());
+            }
+           
+            lbox3.Visibility = Visibility.Visible;
+        }
+
+        private void Lbox3_MouseDoubleClick(object sender, MouseButtonEventArgs e)
+        {
+
+            string str = ((sender as ListBox).SelectedItem as string);
+            secondIndex = (sender as ListBox).SelectedIndex;
+            MatchCollection mc = Regex.Matches(str, @"_[A-Z]+");
+            bool flag = false;
+            foreach (Match match in mc)
+            {
+
+                String c = match.ToString().Substring(1, match.ToString().Length-1);
+                List<string> choice = new List<string>();
+                foreach (string i in help)
+                {
+                    choice.Add(str.Replace(c, i));
+                }
+                lbox4.ItemsSource = choice;
+                flag = true;
+                break;
+            }
+            if (!flag)
+            {
+                List<String> m = new List<String>();
+                m.Add(",");
+                m.Add("\"");
+                m.Add("\'");
+                m.Add(".");
+                m.Add("!");
+                m.Add("?");
+                int diff = 0;
+                if (m.Contains(str[str.Length - 1].ToString())) diff = 1;
+                List<string> choice = new List<string>();
+                foreach (string i in help)
+                {
+                    choice.Add(str.Substring(0,str.Length-diff)+"_"+i+ str.Substring(str.Length-diff,diff));
+                }
+                lbox4.ItemsSource = choice;
+            }
+            tb1.Visibility = Visibility.Visible;
+            bt1.Visibility = Visibility.Visible;
+            lbox4.Visibility = Visibility.Visible;
+            tb1.Text = str;
+        }
+
+        private void Lbox4_MouseDoubleClick(object sender, MouseButtonEventArgs e)
+        {
+            string cs = ((sender as ListBox).SelectedItem as string);
+            lbox3.Items.RemoveAt(secondIndex);
+            lbox3.Items.Insert(secondIndex,cs);
+            string str = "";
+            foreach (var item in lbox3.Items)
+            {
+                str += item.ToString() + " "; 
+            }
+            lbox2.Items.RemoveAt(firstIndex);
+            lbox2.Items.Insert(firstIndex, str);
+            lbox3.Visibility = Visibility.Hidden;
+            lbox4.Visibility = Visibility.Hidden;
+            bt1.Visibility = Visibility.Hidden;
+            tb1.Visibility = Visibility.Hidden;
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            List<string> str = new List<string>();
+            str.AddRange(lbox2.Items.Cast<string>());
+            System.IO.File.WriteAllLines(filepath, str);
+            MessageBox.Show("The file has been saved");
+        }
+
+        private void Bt1_Click(object sender, RoutedEventArgs e)
+        {
+            string cs = tb1.Text;
+            lbox3.Items.RemoveAt(secondIndex);
+            lbox3.Items.Insert(secondIndex, cs);
+            string str = "";
+            foreach (var item in lbox3.Items)
+            {
+                str += item.ToString() + " ";
+            }
+            lbox2.Items.RemoveAt(firstIndex);
+            lbox2.Items.Insert(firstIndex, str);
+            lbox3.Visibility = Visibility.Hidden;
+            lbox4.Visibility = Visibility.Hidden;
+            tb1.Visibility = Visibility.Hidden;
+            bt1.Visibility = Visibility.Hidden;
+        }
+    }
+}
---
+++ Stat.xaml.cs
@@ -0,0 +1,271 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace MyApp
+{
+    /// <summary>
+    /// Interaction logic for Stat.xaml
+    /// </summary>
+    public partial class Stat : Window
+    {
+        string f = "";
+        public class S
+        {
+            public string CS { get; set; }
+            public int Frequency { get; set; }
+        }
+        public class S2
+        {
+            public string CS1 { get; set; }
+            public string CS2 { get; set; }
+            public int Frequency { get; set; }
+        }
+        [Serializable]
+        public class k
+        {
+            public string CS1 { get; set; }
+            public string CS2 { get; set; }
+
+            public override bool Equals(object obj)
+            {
+                if ((obj == null) || !this.GetType().Equals(obj.GetType()))
+                {
+                    return false;
+                }
+                else
+                {
+                    k p = (k)obj;
+                    return (CS1 == p.CS1) && (CS2 == p.CS2);
+                }
+            }
+            public override int GetHashCode()
+            {
+                return CS1.GetHashCode()*2 + CS2.GetHashCode();
+            }
+        }
+
+        public Stat()
+        {
+            InitializeComponent();
+            ShowGrid();
+        }
+        SortedDictionary<String, int> dictionary = new SortedDictionary<string, int>();
+        Dictionary<k, int> d = new Dictionary<k, int>();
+        Dictionary<k, int> d1 = new Dictionary<k, int>();
+        List<String> files = new List<String>();
+        private void Bt1_Click(object sender, RoutedEventArgs e)
+        {
+            String filepath = tb1.Text;
+            filepath = filepath.Substring(0, filepath.Length - 4) + "_painted.txt";
+            string text = File.ReadAllText(filepath);
+            char[] c = { ',', '?', ';', '!', '\"', '\'', '.', ':', '-' };
+            string[] cs = File.ReadAllLines("min_help.txt");
+            for (int i = 0; i < c.Length; i++)
+            {
+
+                int res = text.Count(x => x == c[i]) / 2;
+                Add(ref dictionary, c[i].ToString(), res);
+            }
+            for (int i = 0; i < cs.Length; i++)
+            {
+                string k = cs[i].Split('\t')[0];
+                MatchCollection mc = Regex.Matches(text, cs[i].Split('\t')[0]);
+                foreach (Match match in mc)
+                {
+                    String s = match.ToString();
+                    if (s != "") Add(ref dictionary, s, 1);
+                }
+            }
+            for (int i = 0; i < cs.Length; i++)
+            {
+                for (int j = 0; j < cs.Length; j++)
+                {
+                    string k = cs[i].Split('\t')[0];
+                    MatchCollection mc = Regex.Matches(text, cs[i].Split('\t')[0] + ".+" + cs[j].Split('\t')[0]);
+                    foreach (Match match in mc)
+                    {
+                        String s = match.ToString();
+                        k a = new k();
+                        a.CS1 = cs[i].Split('\t')[0];
+                        a.CS2 = cs[j].Split('\t')[0];
+                        if (s != "") Add1(ref d, a, 1);
+                    }
+                }
+            }
+            run_cmd("chunk.py", tb1.Text);
+            string[] text1 = System.IO.File.ReadAllLines("output.txt");
+            for (int i = 0; i < text1.Length; i++)
+            {
+                string[] t = text1[i].Split(' ');
+                t[0] = t[0].ToLower();
+                k p = new k();
+                p.CS1 = t[0];
+                p.CS2 = t[1];
+                Add1(ref d1, p, 1);
+            }
+            files.Add(tb1.Text);
+            ShowGrid();
+        }
+        public void Add1(ref Dictionary<k, int> l, k c, int v)
+        {
+            if (l.TryGetValue(c, out int h))
+            {
+                l[c] = h + v;
+            }
+            else
+            {
+                l.Add(c, v);
+            }
+        }
+        public void Add(ref SortedDictionary<String, int> l, string c, int v)
+        {
+            if (l.TryGetValue(c, out int h))
+            {
+                l[c] = h + v;
+            }
+            else
+            {
+                l.Add(c, v);
+            }
+        }
+
+        public string run_cmd(string cmd, string args)
+        {
+            ProcessStartInfo start = new ProcessStartInfo();
+            start.FileName = "python.exe";
+            start.Arguments = string.Format("\"{0}\" \"{1}\"", cmd, args);
+            start.UseShellExecute = false;// Do not use OS shell
+            start.CreateNoWindow = true; // We don't need new window
+            start.RedirectStandardOutput = true;// Any output, generated by application will be redirected back
+            start.RedirectStandardError = true; // Any error in standard output will be redirected back (for example exceptions)
+            using (Process process = Process.Start(start))
+            {
+                using (StreamReader reader = process.StandardOutput)
+                {
+                    string stderr = process.StandardError.ReadToEnd(); // Here are the exceptions from our Python script
+                    string result = reader.ReadToEnd(); // Here is the result of StdOut(for example: print "test")
+                    return result;
+                }
+            }
+        }
+
+        public void ShowGrid()
+        {
+            DataGridXAML.ItemsSource = null;
+            DataGridXAML1.ItemsSource = null;
+            DataGridXAML2.ItemsSource = null;
+            DataGridXAML.Items.Clear();
+            DataGridXAML1.Items.Clear();
+            DataGridXAML2.Items.Clear();
+            lb1.ItemsSource = null;
+            lb1.Items.Clear();
+            List<S> l = new List<S>();
+            foreach (string i in dictionary.Keys)
+            {
+                l.Add(new S()
+                {
+                    CS = i,
+                    Frequency = dictionary[i]
+                });
+            }
+            DataGridXAML.ItemsSource = l;
+            List<S2> F = new List<S2>();
+            foreach (k i in d.Keys)
+            {
+                F.Add(new S2()
+                {
+                    CS1 = i.CS1,
+                    CS2 = i.CS2,
+                    Frequency = d[i]
+                });
+            }
+            List<S2> F1 = new List<S2>();
+            foreach (k i in d1.Keys)
+            {
+                F1.Add(new S2()
+                {
+                    CS1 = i.CS1,
+                    CS2 = i.CS2,
+                    Frequency = d1[i]
+                });
+            }
+            DataGridXAML1.ItemsSource = F;
+            DataGridXAML2.ItemsSource = F1;
+            lb1.ItemsSource = files;
+        }
+        private void Bt4_Click(object sender, RoutedEventArgs e)
+        {
+            Serialize();
+            MessageBox.Show("The statistics has been saved");
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            dictionary = new SortedDictionary<String, int>();
+            files = new List<String>();
+            d = new Dictionary<k, int>();
+            ShowGrid();
+        }
+        private void Bt5_Click(object sender, RoutedEventArgs e)
+        {
+            ProcessStartInfo start = new ProcessStartInfo();
+            start.RedirectStandardInput = false;
+            start.FileName = "help.txt";
+            Process.Start(start);
+        }
+
+        private void Window_Initialized(object sender, EventArgs e)
+        {
+            DeSerialize();
+        }
+        public void Serialize()
+        {
+            Stream s = File.Open("stat.dat", FileMode.Create);
+            BinaryFormatter b = new BinaryFormatter();
+            b.Serialize(s, dictionary);
+            s.Close();
+            s = File.Open("lb1.dat", FileMode.Create);
+            b.Serialize(s, files);
+            s.Close();
+            s = File.Open("d.dat", FileMode.Create);
+            b.Serialize(s, d);
+            s.Close();
+            s = File.Open("d1.dat", FileMode.Create);
+            b.Serialize(s, d1);
+            s.Close();
+        }
+        public void DeSerialize()
+        {
+            Stream s = File.Open("stat.dat", FileMode.Open);
+            BinaryFormatter b = new BinaryFormatter();
+            dictionary = (SortedDictionary<String, int>)b.Deserialize(s);
+            s.Close();
+            s = File.Open("lb1.dat", FileMode.Open);
+            files = (List<String>)b.Deserialize(s); 
+            s.Close();
+            s = File.Open("d.dat", FileMode.Open);
+            d = (Dictionary<k,int>)b.Deserialize(s);
+            s.Close();
+            s = File.Open("d1.dat", FileMode.Open);
+            d1 = (Dictionary<k, int>)b.Deserialize(s);
+            s.Close();
+            
+           
+        }
+    }
+}
